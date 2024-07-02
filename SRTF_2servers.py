ff7ed12593bec79ae5c35b91faaaabd52a5745c8
lanceoli <89068321+lanceoli@users.noreply.github.com>
2024-07-02 14:19:37 +0800

    Add files via upload

---
+++ SRTF_2servers.py
@@ -0,0 +1,106 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+df_list = []
+df_list2 = []
+
+arrival = [2, 1, 4, 6, 0]
+burst = [5, 1, 3, 4, 7]
+
+arr_queue_index = []
+burst_queue_index = []  # burstable
+
+queue = []
+queue2 = []
+
+for time in range(sum(burst)):
+
+    for i in range(len(arrival)):
+        if time == arrival[i]:
+            arr_queue_index.append(i)
+            burst_queue_index.append(i)
+
+    # create burstable_values list
+    burstable_values = []
+    for i in range(len(burst_queue_index)):
+        if burst[burst_queue_index[i]] == 0:
+            continue
+        burstable_values.append(burst[burst_queue_index[i]])
+
+    # add queue
+    if len(queue) <= len(queue2):
+      queue.append("P"+str(burst.index(min(burstable_values))+1))
+      df_list.append(dict(Task="P"+str(burst.index(min(burstable_values))+1),
+                          # Start=len(queue)-1,
+                          # End=len(queue)))
+                          Start=time,
+                          End=time+1))
+    # add queue2
+    else:
+      queue2.append("P"+str(burst.index(min(burstable_values))+1))
+      df_list2.append(dict(Task="P"+str(burst.index(min(burstable_values))+1),
+                          # Start=len(queue2)-1,
+                          # End=len(queue2)))
+                          Start=time,
+                          End=time+1))
+
+    filter(None, burstable_values)
+    burst[burst.index(min(burstable_values))] = (
+            burst[burst.index(min(burstable_values))] - 1)
+
+    # print
+    print("Time:", time)
+    print("Queue:", queue)
+    print("Queue2:", queue2)
+    print("Burstable:", burstable_values)
+
+    if burstable_values[len(burstable_values) - 1] == 0:
+        burstable_values[len(burstable_values) - 1] = None
+
+# plot timeline
+df = pd.DataFrame(df_list, columns=['Task', 'Start', 'End'])
+print(df)
+
+df2 = pd.DataFrame(df_list2, columns=['Task', 'Start', 'End'])
+print(df2)
+
+persons_set = set(name.strip() for names in df['Task'] for name in names.split(","))
+persons_set2 = set(name.strip() for names in df2['Task'] for name in names.split(","))
+
+persons = {p: i for i, p in enumerate(sorted(persons_set))}
+if len(persons_set) < len(persons_set2):
+  persons = {p: i for i, p in enumerate(sorted(persons_set2))}
+
+for person in persons:
+    periods = []
+    for names, start, end in zip(df['Task'], df['Start'], df['End']):
+        if person in set(name.strip() for name in names.split(",")):
+            periods.append((start, end - start))
+    plt.broken_barh(periods, (persons[person] - 0.45, 0.9),
+                    facecolors=('tab:blue'))
+    
+for person in persons:
+    periods = []
+    for names, start, end in zip(df2['Task'], df2['Start'], df2['End']):
+        if person in set(name.strip() for name in names.split(",")):
+            periods.append((start, end - start))
+    plt.broken_barh(periods, (persons[person] - 0.45, 0.9),
+                    facecolors=('tab:green'))
+
+plt.xticks(range(len(queue)*2+1))
+plt.yticks(range(len(persons)), persons)
+# if len(persons2) > len(persons):
+#   plt.yticks(range(len(persons2)), persons2)
+plt.xlabel("Time")
+plt.ylabel("Jobs")
+plt.title("Shortest-Remaining-Time-First Scheduling Algorithm")
+plt.legend(loc="upper left")
+plt.gca().legend(('server 1','server 2'))
+ax = plt.gca()
+leg = ax.get_legend()
+leg.legendHandles[0].set_color('blue')
+leg.legendHandles[1].set_color('green')
+plt.gca().invert_yaxis()
+plt.show()
